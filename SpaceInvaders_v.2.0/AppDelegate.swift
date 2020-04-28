e83bce630d8f5d0ce96b3d5bfade42003824c349
Ocean1878 <sikefayati@icloud.com>
2020-04-28 20:06:16 +0200

    Initial Commit

---
+++ SpaceInvaders_v.2.0/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SpaceInvaders_v.2.0
+//
+//  Created by Iman Kefayati on 28.04.20.
+//  Copyright © 2020 Iman Kefayati. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ SpaceInvaders_v.2.0UITests/SpaceInvaders_v_2_0UITests.swift
@@ -0,0 +1,43 @@
+//
+//  SpaceInvaders_v_2_0UITests.swift
+//  SpaceInvaders_v.2.0UITests
+//
+//  Created by Iman Kefayati on 28.04.20.
+//  Copyright © 2020 Iman Kefayati. All rights reserved.
+//
+
+import XCTest
+
+class SpaceInvaders_v_2_0UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
